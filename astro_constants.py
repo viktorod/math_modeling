111e5424a89e7fa5e4aa6bdd22a18c39761bea7a
viktorod <66479433+viktorod@users.noreply.github.com>
2020-06-29 13:59:18 +0200

    Add files via upload

---
+++ astro_constants.py
@@ -0,0 +1,12 @@
+earth_radius = 6378.14 #km
+earth_mass = 5.974*10**24 #kg
+earth_year = 365,24219 #day
+
+sun_radius = 697000 #km
+sun_mass = 1.989*10**30 #kg
+
+planck_const = 6.626*10**(-34) #kg*(m**2)*(s**(-1))
+graviti_const = 6.672*10**(-11) #(m**3)*(kg**(-1))*(s**(-2))
+light_speed = 2.998*10**8 #m/s
+astro_unit = 1.496*10*8 #km
+parsec = 206265 #au
