2ee3d53eebfd7f920a74db17a13f07afcf2582f1
viktorod <66479433+viktorod@users.noreply.github.com>
2020-07-01 16:22:23 +0200

    Update lab_3_pr_2.py

--- lab_3_pr_2.py
+++ lab_3_pr_2.py
@@ -8,12 +8,3 @@ h = 100
 υ = ((g * h * np.tan(β)**2) / (2 * np.cos(α)**2 * (1 - np.tan(β) * np.tan(α))))**0.5
 
 print(υ)
-
-from scipy.constants import g, e, h, k
-from math import pi
-T = 200
-ε = 300
-
-N = ((2 / (pi**0.5))*(h / ((k*T)**1.5))*(e ** ((-ε) / k*T))*(ε**(T/2)))
-     
-print(N)
