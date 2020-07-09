7cf750764107dbe2cf5a0cab67a78da4f85fa3ed
viktorod <66479433+viktorod@users.noreply.github.com>
2020-07-09 16:41:24 +0200

    Add files via upload

---
+++ astro_constants.py
@@ -0,0 +1,28 @@
+graviti_const = G =6.672*10**(-11) #(m**3)*(kg**(-1))*(s**(-2)) - Гравитационная постоянная
+light_speed = c = 2.998*10**8 #m/s - Скорость света в вакууме
+graviti_acc = g = 9.8 #m/s**2 - ускорение свободного падения
+boltz_const = k = 1.38 * 10**(-23) #Дж/К - постоянная Больцмана
+planck_const = h = 6.626*10**(-34) #kg*(m**2)*(s**(-1)) - постоянная Планка
+euler_number= e = 2.72 # - Число Эйлера
+astro_unit = 1.496*10**11 #m
+parsec = 206265 #au
+
+sun_mass = 1.989*10**30 #kg
+mercury_mass = 3.302*10**23 #kg
+venus_mass = 4.869*10**24 #kg
+earth_mass = 5.974*10**24 #kg 
+mars_mass = 6.419*10**23 #kg
+jupiter_mass = 1.899*10**27 #kg
+saturn_mass = 5.685*10**26 #kg
+uranus_mass = 8.683*10**25 #kg
+neptune_mass = 1.024*10**26 #kg
+
+sun_radius = 6.97*10**8 #m
+mercury_radius = 2.4397*10**6 #m
+venus_radius = 6.0518*10**6 #m
+earth_radius = 6.3781*10**6 #m
+mars_radius = 3.3972*10**6 #m
+jupiter_radius = 7.1492*10**7 #m
+saturn_radius = 6.0268*10**7 #m
+uranus_radius = 2.5559*10**7 #m
+neptune_radius = 2.4746*10**7 #m
