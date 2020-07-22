e200a25d121c6deb1420d17b4d8a7c2234e44e3f
viktorod <66479433+viktorod@users.noreply.github.com>
2020-07-22 15:33:55 +0200

    Add files via upload

---
+++ lab_5_pr_1.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+x = [1,1,5,5,1]
+y = [1,5,5,1,1]
+plt.plot(x,y,color='k', marker='o', ms=5)
+plt.xlabel('coord x')
+plt.ylabel('coord y')
+plt.title('square')
+plt.legend()
+plt.grid()
+plt.axis('equal')
+
+plt.show()
