d38ea61d85e74a3f5cfa6e5fab6c9ce9d48db06d
viktorod <66479433+viktorod@users.noreply.github.com>
2020-07-29 14:45:42 +0200

    Add files via upload

---
+++ lab_6_pr_1.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def cycloid_and_astroid(R=2):
+    
+    t = np.linspace(0, 2*np.pi,100)
+    x = R*(t-np.sin(t))
+    y = R*(1-np.cos(t))
+    
+    plt.axis('equal')
+    plt.plot(x,y)
+    plt.xlabel('coord x')
+    plt.ylabel('coord y')
+    plt.title('cycloid')
+    
+    plt.show()
+           
+    t = np.linspace(0, 2*np.pi,100)
+    
+    x= R*np.cos(t)**3
+    y= R*np.sin(t)**3
+    
+    plt.axis('equal')
+    plt.plot(x,y)
+    plt.xlabel('coord x')
+    plt.ylabel('coord y')
+    plt.title('astroid')
+
+cycloid_and_astroid()
