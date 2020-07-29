9e02bfa82fb7874cc52b286a83105f5fcafadca4
viktorod <66479433+viktorod@users.noreply.github.com>
2020-07-29 14:56:24 +0200

    Update lab_6_pr_1.py

--- lab_6_pr_1.py
+++ lab_6_pr_1.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def cycloid_and_astroid(R=2):
-    
+    """ Функция строит Циклоиду и Астроиду """
     t = np.linspace(0, 2*np.pi,100)
     x = R*(t-np.sin(t))
     y = R*(1-np.cos(t))
@@ -11,6 +11,7 @@ def cycloid_and_astroid(R=2):
     plt.plot(x,y)
     plt.xlabel('coord x')
     plt.ylabel('coord y')
+    plt.grid()
     plt.title('cycloid')
     
     plt.show()
@@ -24,6 +25,7 @@ def cycloid_and_astroid(R=2):
     plt.plot(x,y)
     plt.xlabel('coord x')
     plt.ylabel('coord y')
+    plt.grid
     plt.title('astroid')
 
 cycloid_and_astroid()
