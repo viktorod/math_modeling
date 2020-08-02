0c5a050e44188d073bfb4d4b406a30f38177a096
viktorod <66479433+viktorod@users.noreply.github.com>
2020-08-02 14:31:38 +0200

    Add files via upload

---
+++ lab_5_pr_2.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def parabola (a,b,c,X,N):
+    """функция пытается строить параболу,
+       подайте a, b и c, так же пределы и 
+       кол-во точек"""
+
+    x = np.linspace(-X, X, N)
+    
+    y=a*x**2+b*x+c
+    
+    plt.plot(x,y,label='y=a*x**2+b*x+c')
+    plt.xlabel('coord - x')
+    plt.ylabel('coord - y')
+    plt.axis('equal')
+    plt.title('парабола')
+    plt.legend()
+    
+    plt.show()
+    
+parabola(1,0,0,3,100)
+
+
+def hyperbola(k, X, N):
+    """Функция умела строить гиперболу,
+       нужно подать k, снова пределы и
+       кол-во точек"""
+    
+    x = np.linspace(-X, X ,N)
+    
+    y = k/x
+    plt.plot(x,y, label='y = k/x')
+
+    
+    plt.title('Giperbola')
+    plt.xlabel('Coord - x')
+    plt.ylabel('Coord - y')
+    plt.title('гипербола')
+    plt.legend()
+    plt.grid() 
+    
+hyperbola(1,5,100)
